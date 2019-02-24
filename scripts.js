df4228a17b501e4ca785f961fdb024525ea8a7c0
natedebenedetti <natedebenedetti@gmail.com>
2019-02-24 10:48:54 -0500

    finished with promise chaining, next is Async/Await

---
+++ scripts.js
@@ -0,0 +1,37 @@
+// step 2: Create promise chain using slow math.
+
+slowMath.add(6, 2).then((answer) => { //all of the console logs will wait 1 second to log due to slow math's promises being written with a setTimeout of 1000.
+    console.log(answer); // log's the "answer" for adding 6 + 2.
+    return slowMath.multiply(answer, 2); // multiplies "answer" by 2.
+}).then((answer) => {
+    console.log(answer); // log's new value of "answer".
+    return slowMath.divide(answer, 4); // divides "answer" by 4.
+}).then((answer) => {
+    console.log(answer); //log's new value of "answer".
+    return slowMath.subtract(answer, 3); // subtracts 3 from "answer".
+}).then((answer) => {
+    console.log(answer); //log's new value of "answer".
+    return slowMath.add(answer, 98); //adds 98 to "answer".
+}).then((answer) => {
+    console.log(answer); //log's new value of "answer".
+    return slowMath.remainder(answer, 2); //finds the remainder of dividing "answer" by 2.
+}).then((answer) => {
+    console.log(answer); //log's new value of "answer".
+    return slowMath.multiply(answer, 50); // multiplies "answer" by 50.
+}).then((answer) => {
+    console.log(answer); //log's new value of "answer".
+    return slowMath.remainder(answer, 40); //finds the remainder of dividing "answer" by 40.
+}).then((answer) => { 
+    console.log(answer); //log's new value of "answer".
+    return slowMath.add(answer, 32); //adds 32 to "answer"
+}).then((answer) => {
+    console.log(`The final result is ${answer}`); //log's new value of "answer" concatenated onto the end of the string.
+}).catch((error) => { // provides a catch to handle any errors that may arise.
+    console.log(error); // logs the error to the console.
+});
+//step 3: opening index.html in browser displays all "answer"'s correctly and results in expected final value of 42.
+//step 4: changing the first step to add 1 and 1 together results in failure at js:11 because the value becomes a negative number at this point and slowMath promises are written to throw errors is negative numbers are involved. 
+
+
+//ASYNC/AWAIT
+
